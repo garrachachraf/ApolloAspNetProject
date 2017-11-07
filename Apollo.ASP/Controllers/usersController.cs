60b9be45bc497aa140607cfba807cfd69b62b4bc
maissen ayed <ayed.maissen@gmail.com>
2017-11-07 18:32:54 +0100

    user controller

---
+++ Apollo.ASP/Controllers/usersController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Apollo.Data;
+using Apollo.Domain.entities;
+
+namespace Apollo.ASP.Controllers
+{
+    public class usersController : Controller
+    {
+        private JeeModel db = new JeeModel();
+
+        // GET: users
+        public ActionResult Index()
+        {
+            var user = db.user.Include(u => u.collection).Include(u => u.whishlist);
+            return View(user.ToList());
+        }
+
+        // GET: users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: users/Create
+        public ActionResult Create()
+        {
+            ViewBag.collection_id = new SelectList(db.collection, "id", "description");
+            ViewBag.whishList_id = new SelectList(db.whishlist, "id", "id");
+            return View();
+        }
+
+        // POST: users/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,role,city,country,email,firstname,gender,imagePath,lastname,password,state,street,userName,zipCode,PhoneNumber,type,collection_id,whishList_id")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.user.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.collection_id = new SelectList(db.collection, "id", "description", user.collection_id);
+            ViewBag.whishList_id = new SelectList(db.whishlist, "id", "id", user.whishList_id);
+            return View(user);
+        }
+
+        // GET: users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.collection_id = new SelectList(db.collection, "id", "description", user.collection_id);
+            ViewBag.whishList_id = new SelectList(db.whishlist, "id", "id", user.whishList_id);
+            return View(user);
+        }
+
+        // POST: users/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,role,city,country,email,firstname,gender,imagePath,lastname,password,state,street,userName,zipCode,PhoneNumber,type,collection_id,whishList_id")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.collection_id = new SelectList(db.collection, "id", "description", user.collection_id);
+            ViewBag.whishList_id = new SelectList(db.whishlist, "id", "id", user.whishList_id);
+            return View(user);
+        }
+
+        // GET: users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            user user = db.user.Find(id);
+            db.user.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
